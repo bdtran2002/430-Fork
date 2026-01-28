90d669e519c91caacfa022b7c2f131a0221843a8
Brandon Tran <bdtran2002@gmail.com>
2026-01-28 11:43:07 -0800

    Prevents division by 0

--- main.cpp
+++ main.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 #include <cmath>
 
-int main()
-{
-    std::cout << "THE FIRST EXAMPLE MATH DISPLAY!\n";
-    std::cout << "Hi, please enter two whole numbers: ";
+int main(){
+	std::cout << "THE FIRST EXAMPLE MATH DISPLAY!\n";
+	std::cout << "Hi, please enter two whole numbers: ";
 
-    int x,y;
+	int x,y;
+	std::cin >> x >> y;
+	if (x == 0 || y == 0) {
+		std::cout << "Error: Division or remainder by zero is not allowed." << std::endl;
+		return 0;
+	}
+	std::cout << "Addition: " << x + y << std::endl;
+	std::cout << "Subtraction: " << x - y << std::endl;
+	std::cout << "Multiplication: " << x * y << std::endl;
+	std::cout << "Division: " << x / y << std::endl;
+	std::cout << "Remainder: " << x % y << std::endl;
+	std::cout << "Square Root: " << sqrt(x) << std::endl;
+	std::cout << "Square: " << pow(x, y) << std::endl;
 
-    std::cin >> x >> y;
-    std::cout << "Addition: " << x + y << std::endl;
-    std::cout << "Subtraction: " << x - y << std::endl;
-    std::cout << "Multiplication: " << x * y << std::endl;
-    std::cout << "Division: " << x / y << std::endl;
-    std::cout << "Remainder: " << x % y << std::endl;
-    std::cout << "Square Root: " << sqrt(x) << std::endl;
-    std::cout << "Square: " << pow(x, y) << std::endl;
-
-    return 0;
+	return 0;
 }
